88aa1a0047c3468ee976eb346857710da71d731f
unknown <ruinz1>
2022-07-16 14:07:31 +0800

    portofolio

---
+++ src/js/script.js
@@ -0,0 +1,23 @@
+//Navbar Fix
+window.onscroll = function(){
+    const header = document.querySelector('header');
+    const fixnav = header.offsetTop;
+
+    if(window.pageYOffset > fixnav){
+        header.classList.add('navbar-fixed');
+    }
+    else
+    {
+        header.classList.remove('navbar-fixed');
+    }
+}
+
+// Hamburger
+
+const hamburger = document.querySelector('#hamburger');
+const navmenu = document.querySelector('#nav-menu');
+
+hamburger.addEventListener('click', function() {
+    hamburger.classList.toggle('hamburger-active');
+    navmenu.classList.toggle('hidden');
+});
